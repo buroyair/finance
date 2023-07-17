7bdf453c3ab21f158ff04b91bfaf00970b0cbdab
buroyair <buroyair@gmail.com>
2023-07-17 17:07:16 +0300

    review

--- Option Pricing/BS_Model.py
+++ Option Pricing/BS_Model.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import norm
 
+
+
 # Black-Scholes formula
 def black_scholes_call(S, K, T, r, sigma):
     d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
